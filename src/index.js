90691c08e4ea3ae16a5a52b7fef5601e5edac630
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-06-26 09:47:36 +0700

    config for pug

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-const express = require("express")  ; 
+const express = require("express");
+const path = require("path");
 
-const port = 3000 ; 
-const app = express() ;
+const port = 3000;
+const app = express();
 
-app.get( "/" , (req, res) =>{
-  res.send("hello word 1111 11111  hhgj") ;
-})
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
 
-app.listen(port , () =>{
+app.get("/", (req, res) => {
+  res.render("index.pug", {
+    title: "trang chu",
+    message: "Chào bạn đến với Pug ",
+  });
+});
+
+app.listen(port, () => {
   console.log(`Example listener on port ${port} `);
-})
+});
