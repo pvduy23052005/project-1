5ac3f4ea6594e39e2371979cd04e150ab4f44084
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-07-01 15:16:47 +0700

    config folder for pug

--- src/controllers/client/product.controller.js
+++ src/controllers/client/product.controller.js
@@ -2,7 +2,7 @@
 
 module.exports.index =  (req, res) => {
   res.render("client/pages/product/index" ,{
-    
+    title : "Product"
   }) ;
 }
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const clientRoute = require("./routes/client/index.route");
+require("dotenv").config(); // load biến từ .env
 
 const uri =
   "mongodb+srv://phungvanduy23052005:trpt1D9TU6BllRh6@product-managerment.webbk.mongodb.net/?retryWrites=true&w=majority&appName=product-managerment";
-const port = 3000;
+const port = process.env.PORT;
 const app = express();
 
 app.set("view engine", "pug");
