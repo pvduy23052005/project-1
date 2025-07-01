85f82480cd41ef59a3816c80d9605d6e40894c8c
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-07-01 16:49:06 +0700

    layout for client

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ const mongoose = require("mongoose");
 const clientRoute = require("./routes/client/index.route");
 require("dotenv").config(); // load biến từ .env
 
-const uri =
-  "mongodb+srv://phungvanduy23052005:trpt1D9TU6BllRh6@product-managerment.webbk.mongodb.net/?retryWrites=true&w=majority&appName=product-managerment";
+const uri ="mongodb+srv://phungvanduy23052005:trpt1D9TU6BllRh6@product-managerment.webbk.mongodb.net/?retryWrites=true&w=majority&appName=product-managerment";
 const port = process.env.PORT;
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "./views"));
-app.use("/static", express.static(path.join(__dirname, "public")));
 
+app.use("/", express.static(path.join(__dirname, "public")));
 
+//client route 
 clientRoute(app);
 
 app.listen(port, () => {
