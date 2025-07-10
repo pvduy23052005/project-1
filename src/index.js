bb6e967bc58e8b92f99ef6a82ea033fb226c7557
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-07-10 10:29:36 +0700

    update vercel

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ const adminRoute = require("./routes/admin/index.route");
 require("dotenv").config(); // load biến từ .env
 const database = require("./config/database");
 const systemConfig = require("./config/system");
-const methodOverride = require('method-override');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const flash = require('express-flash'); 
-const serverless = require('serverless-http');
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const flash = require("express-flash");
+const serverless = require("serverless-http");
 
 const port = process.env.PORT;
 const app = express();
@@ -18,22 +18,24 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "./views"));
 app.use("/", express.static(path.join(__dirname, "public")));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(session({
-  secret: 'your-secret-key', // Replace with a strong, unique secret key
-  resave: false,
-  saveUninitialized: true,
-  cookie: { maxAge: 60000 } // Example: session lasts for 1 minute
-}));
+app.use(
+  session({
+    secret: "your-secret-key", // Replace with a strong, unique secret key
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 }, // Example: session lasts for 1 minute
+  })
+);
 app.use(flash());
 
 app.use((req, res, next) => {
-  res.locals.success = req.flash('success');
-  res.locals.error = req.flash('error');
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   next();
 });
 
@@ -45,8 +47,11 @@ database.connectDatabase(); // kết nối database
 clientRoute(app);
 adminRoute(app);
 
-app.listen(port, () => {
-  console.log(`Example listener on port ${port} `);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(port, () => {
+    console.log(`Local server is running on http://localhost:${port}`);
+  });
+}
 
+module.exports = app;
 module.exports.handler = serverless(app);
